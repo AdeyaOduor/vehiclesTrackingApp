d02a6e84dc347548005e2037694654fa84f511b7
adeyadavid <adeyadavid@gmail.com>
2024-04-25 16:40:37 +0300

    modified add_vehicles views.py file

--- vehicleTrackingApp/add_vehicles/views.py
+++ vehicleTrackingApp/add_vehicles/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, redirect
-from vehicles.models import Category, vehicles
+from .models import Category, Vehicle
 from .forms import SignupForm
 
 def index(request):
-    vehicles = vehicles.objects.filter(is_ticketed=False)[0:3]
+    vehicles_list = Vehicle.objects.filter(is_ticketed=False)[0:3]
     categories = Category.objects.all()
-    return render(request, add_vehicles/'index.html', {'categories': categories, 'vehicles': vehicles,
-    })
-    
+    return render(request, 'add_vehicles/index.html', {'categories': categories, 'vehicles': vehicles_list})
+
 def tickets(request):
-    return render(request, add_vehicles/'tickets.html')
-    
+    return render(request, 'add_vehicles/tickets.html')
+
 def signup(request):
-    if request.method == 'POST': 
+    if request.method == 'POST':
         form = SignupForm(request.POST)
         
-     if form.is_valid(): 
-        form.save()  
-        return redirect('/login/')  
-     else:
-     form = SignupForm()
-    return render(request, add_vehicles/'signup.html', {
-          'form': form
-    }) 
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
+    return render(request, 'add_vehicles/signup.html', {'form': form})
