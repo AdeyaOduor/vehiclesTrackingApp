f978ff86e21f2b6059b6e5d223bbd73d58ff7b0c
adeyadavid <adeyadavid@gmail.com>
2024-04-25 16:28:49 +0300

    modified add_vehicles url.py file

--- vehicleTrackingApp/add_vehicles/urls.py
+++ vehicleTrackingApp/add_vehicles/urls.py
@@ -9,5 +9,5 @@ urlspatterns = [
      path('', views.index, name='index'),
      path('tickets/',views.tickets, make='tickets'),
      path('signup/',views.signup, make='signup'),
-     path('login/',auth_views.loginview.as_view(template_make='add_vehicles/login.html'authentication_form=LoginForm), make='login'),
+     path('login/', auth_views.loginview.as_view(template_make='add_vehicles/login.html', authentication_form=LoginForm), make='login'),
 ]
