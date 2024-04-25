7cb007e9a496a3f9668384e16b8468fd78c6090c
adeyadavid <adeyadavid@gmail.com>
2024-04-25 16:09:55 +0300

    modified model files

--- vehicleTrackingApp/ticketing/models.py
+++ vehicleTrackingApp/ticketing/models.py
@@ -1,22 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
-from vehicles.models import vehicles
+from vehicles.models import Vehicle
 
-class Ticketing(models.model): 
-      vehicles = models.ForeignKey(vehicles, related_make='ticketing', on_delete=models.CASCADE)
-      members =models.ManyToManyFields(User, related_make='ticketing')
-      created_at = models.DateTimeField(auto_now_add=True)
-      modified_at = models.DateTimeField(auto_now=True)
-      
-      class Meta:
-          placement = ('-modified_at',)
+class Ticketing(models.Model):
+    vehicle = models.ForeignKey(Vehicle, related_name='ticketing', on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='ticketing')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
-class TicketingForm(models.model):
-       ticketing = models.foreignKey(Ticketing, related_make='tickets', on_delete=models.CASCADE)
-       origin = models.CharField()
-       departure_date = models.CharField()
-       destination = models.CharField()
-       milage_km = models.CharField()
-       return_date = models.CharField()
-       created_at = models.DateTimeField(auto_now_add=True)
-       created_by = models.ForeignKey(User, related_make='created_tickets', on_delete=models.CASCADE)
+    class Meta:
+        ordering = ('-modified_at',)
+
+class TicketingForm(models.Model):
+    ticketing = models.ForeignKey(Ticketing, related_name='tickets', on_delete=models.CASCADE)
+    origin = models.CharField(max_length=255)
+    departure_date = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    mileage_km = models.CharField(max_length=255)
+    return_date = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='created_tickets', on_delete=models.CASCADE)
