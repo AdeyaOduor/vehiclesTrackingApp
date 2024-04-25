c12105b31d27d3f39a477bec399631ab0f50cca4
adeyadavid <adeyadavid@gmail.com>
2024-04-25 16:17:27 +0300

    modified ticketing admin.py file

--- vehicleTrackingApp/ticketing/admin.py
+++ vehicleTrackingApp/ticketing/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Ticketing, TicketingForm
 
 admin.site.register(Ticketing)
-admin.site.register(Ticketingtickets)
+admin.site.register(TicketingForm)
