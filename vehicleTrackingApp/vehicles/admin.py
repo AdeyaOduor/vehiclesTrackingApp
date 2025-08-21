9f7704777feeac28dd5ba7911cbbc0feca79bd4a
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-08-21 11:39:05 +0300

    Update admin.py

--- vehicleTrackingApp/vehicles/admin.py
+++ vehicleTrackingApp/vehicles/admin.py
@@ -1,5 +1,28 @@
-from django.contrib import admin
-from .models import Category, Vehicle
+# from django.contrib import admin
+# from .models import Category, Vehicle
 
-admin.site.register(Category)
-admin.site.register(Vehicle)
+# admin.site.register(Category)
+# admin.site.register(Vehicle)
+
+# admin.py
+from django.contrib import admin
+from .models import *
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'user_type', 'county', 'subcounty']
+    list_filter = ['user_type', 'county', 'subcounty']
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['license_plate', 'make', 'model', 'county', 'subcounty', 'status']
+    list_filter = ['vehicle_type', 'status', 'county', 'subcounty']
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'latitude', 'longitude', 'timestamp']
+    list_filter = ['vehicle__county', 'vehicle__subcounty']
+
+admin.site.register(WorkTicket)
+admin.site.register(MaintenanceSchedule)
+admin.site.register(Fueling)
