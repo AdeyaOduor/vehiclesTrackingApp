f8c275115b3640b177cde29c65e6c5b33089a328
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-08-22 14:19:19 +0300

    Update forms.py

--- vehicleTrackingApp/vehicles/forms.py
+++ vehicleTrackingApp/vehicles/forms.py
@@ -1,45 +1,88 @@
+# from django import forms
+# from .models import Vehicle
+
+# INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+# class NewVehiclesForm(forms.ModelForm):
+#     class Meta:
+#         model = Vehicle
+#         fields = ('category', 'make', 'description', 'registration', 'image',)
+#         widgets = {
+#             'category': forms.Select(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'make': forms.TextInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'description': forms.Textarea(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'registration': forms.TextInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'image': forms.FileInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#         }
+
+# class EditVehiclesForm(forms.ModelForm):
+#     class Meta:
+#         model = Vehicle
+#         fields = ('make', 'description', 'registration', 'image', 'is_ticketed',)
+#         widgets = {
+#             'make': forms.TextInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'description': forms.Textarea(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'registration': forms.TextInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#             'image': forms.FileInput(attrs={
+#                 'class': INPUT_CLASSES
+#             }),
+#         }
+
+
+# forms.py
 from django import forms
-from .models import Vehicle
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, Vehicle, WorkTicket, MaintenanceSchedule, Fueling
 
-INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'user_type', 'county', 'subcounty', 'phone_number')
 
-class NewVehiclesForm(forms.ModelForm):
+class VehicleForm(forms.ModelForm):
     class Meta:
         model = Vehicle
-        fields = ('category', 'make', 'description', 'registration', 'image',)
+        fields = '__all__'
+        exclude = ['current_driver']
+
+class WorkTicketForm(forms.ModelForm):
+    class Meta:
+        model = WorkTicket
+        fields = ['ticket_type', 'description', 'scheduled_date']
         widgets = {
-            'category': forms.Select(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'make': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'description': forms.Textarea(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'registration': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'image': forms.FileInput(attrs={
-                'class': INPUT_CLASSES
-            }),
+            'scheduled_date': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 3}),
         }
 
-class EditVehiclesForm(forms.ModelForm):
+class MaintenanceScheduleForm(forms.ModelForm):
     class Meta:
-        model = Vehicle
-        fields = ('make', 'description', 'registration', 'image', 'is_ticketed',)
+        model = MaintenanceSchedule
+        fields = ['maintenance_type', 'description', 'scheduled_date']
         widgets = {
-            'make': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'description': forms.Textarea(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'registration': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-            }),
-            'image': forms.FileInput(attrs={
-                'class': INPUT_CLASSES
-            }),
+            'scheduled_date': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class FuelingForm(forms.ModelForm):
+    class Meta:
+        model = Fueling
+        fields = ['liters', 'cost', 'mileage', 'station_name', 'station_location']
+        widgets = {
+            'date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
