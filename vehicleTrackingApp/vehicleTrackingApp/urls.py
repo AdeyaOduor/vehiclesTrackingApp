1ee7e7fb1236518c2b68708ac55268421cb51f11
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-08-23 11:46:21 +0300

    Update urls.py

--- vehicleTrackingApp/vehicleTrackingApp/urls.py
+++ vehicleTrackingApp/vehicleTrackingApp/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 
@@ -26,5 +27,9 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('ticketing/', include('ticketing.urls')),
     path('admin/', admin.site.urls),
+    path('', include('vehicle_tracking.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Points at media settings.py {avoid in production}
+
