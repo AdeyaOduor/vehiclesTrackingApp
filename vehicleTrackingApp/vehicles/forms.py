df5d94bffe14514cd762bed8a1ec53dbcfe643b5
ADEYA DAVID ODUOR <adeyadavid@gmail.com>
2025-08-23 11:38:06 +0300

    Update forms.py

--- vehicleTrackingApp/vehicles/forms.py
+++ vehicleTrackingApp/vehicles/forms.py
@@ -1,7 +1,20 @@
-# from django import forms
-# from .models import Vehicle
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, Vehicle, WorkTicket, MaintenanceSchedule, Fueling
 
-# INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'user_type', 'county', 'subcounty', 'phone_number')
+
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+        exclude = ['current_driver']
 
 # class NewVehiclesForm(forms.ModelForm):
 #     class Meta:
@@ -24,42 +37,6 @@
 #                 'class': INPUT_CLASSES
 #             }),
 #         }
-
-# class EditVehiclesForm(forms.ModelForm):
-#     class Meta:
-#         model = Vehicle
-#         fields = ('make', 'description', 'registration', 'image', 'is_ticketed',)
-#         widgets = {
-#             'make': forms.TextInput(attrs={
-#                 'class': INPUT_CLASSES
-#             }),
-#             'description': forms.Textarea(attrs={
-#                 'class': INPUT_CLASSES
-#             }),
-#             'registration': forms.TextInput(attrs={
-#                 'class': INPUT_CLASSES
-#             }),
-#             'image': forms.FileInput(attrs={
-#                 'class': INPUT_CLASSES
-#             }),
-#         }
-
-
-# forms.py
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser, Vehicle, WorkTicket, MaintenanceSchedule, Fueling
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'user_type', 'county', 'subcounty', 'phone_number')
-
-class VehicleForm(forms.ModelForm):
-    class Meta:
-        model = Vehicle
-        fields = '__all__'
-        exclude = ['current_driver']
 
 class WorkTicketForm(forms.ModelForm):
     class Meta:
@@ -85,4 +62,23 @@ class FuelingForm(forms.ModelForm):
         fields = ['liters', 'cost', 'mileage', 'station_name', 'station_location']
         widgets = {
             'date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+class EditVehiclesForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ('make', 'description', 'registration', 'image', 'is_ticketed',)
+        widgets = {
+            'make': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'registration': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
         }
